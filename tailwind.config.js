3ab224a070db877e38a8580d7a9cf430c7e23521
西川雅人 <masato.nishikawa@arteryex.biz>
2025-04-25 15:36:58 +0900

    Youtubeのチュートリアル完成 ＊背景画像は直接リンクでの対応

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "tutorial-bg": "url('../src/img/main-bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
